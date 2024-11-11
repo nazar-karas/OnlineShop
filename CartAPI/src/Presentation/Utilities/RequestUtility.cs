b4d69bb69837a25305853f1659ee22c71829e596
nazar-karas <nazar.karas@hebronsoft.com>
2024-11-11 15:04:10 +0200

    Added Presentation Layer functionality

---
+++ CartAPI/src/Presentation/Utilities/RequestUtility.cs
@@ -0,0 +1,28 @@
+﻿using Application.Requests.Queries;
+using static MassTransit.ValidationResultExtensions;
+using System.Text.Json;
+using System.Web;
+
+namespace Presentation.Utilities
+{
+    public class RequestUtility
+    {
+        public static T ParseQuery<T>(string queryString)
+        {
+            if (string.IsNullOrEmpty(queryString))
+            {
+                return default(T);
+            }
+
+            var input = HttpUtility.ParseQueryString(queryString);
+
+            var dictionary = input.Cast<string>().ToDictionary(x => x, y => input[y]);
+
+            string json = JsonSerializer.Serialize(dictionary);
+
+            var result = JsonSerializer.Deserialize<T>(json);
+
+            return result;
+        }
+    }
+}
